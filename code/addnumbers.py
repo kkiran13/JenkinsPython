0120464129d30f2bda2a1511292fed71977d90e4
Karthik Kiran <kkiran@Karthik-Kirans-MacBook-Pro.local>
2016-11-11 14:34:48 -0500

    first commit

---
+++ code/addnumbers.py
@@ -0,0 +1,14 @@
+
+
+class AddNumbers:
+
+    def __init__(self):
+        self.a = 10
+
+    def add_numbers(self, b):
+        return self.a + b
+
+if __name__ == '__main__':
+    obj = AddNumbers()
+    print obj.add_numbers(2)
+
---
+++ test/addtest.py
@@ -0,0 +1,20 @@
+import unittest
+import xmlrunner
+
+
+class RunAddTest(unittest.TestCase):
+
+    def test_pass(self):
+        self.assertEqual(12, 8 + 4)
+
+    def test_fail(self):
+        self.assertNotEqual(12, 8 + 3)
+
+    # def test_pass(self):
+    #     self.assertEqual(AddNumbers().add_numbers(2), 12)
+    #
+    # def test_fail(self):
+    #     self.assertNotEqual(AddNumbers().add_numbers(2), 11)
+
+if __name__ == '__main__':
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))

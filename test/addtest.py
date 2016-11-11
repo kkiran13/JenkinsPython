54b842e456110411cce178eb4635b5cbbece649f
Karthik Kiran <kkiran@Karthik-Kirans-MacBook-Pro.local>
2016-11-11 15:21:52 -0500

    add more tests

--- test/addtest.py
+++ test/addtest.py
@@ -1,5 +1,6 @@
 import unittest
 import xmlrunner
+from code.addnumbers import AddNumbers
 
 
 class RunAddTest(unittest.TestCase):
@@ -10,11 +11,11 @@ class RunAddTest(unittest.TestCase):
     def test_fail(self):
         self.assertNotEqual(12, 8 + 3)
 
-    # def test_pass(self):
-    #     self.assertEqual(AddNumbers().add_numbers(2), 12)
-    #
-    # def test_fail(self):
-    #     self.assertNotEqual(AddNumbers().add_numbers(2), 11)
+    def test_add_pass(self):
+        self.assertEqual(AddNumbers().add_numbers(2), 12)
+
+    def test_add_fail(self):
+        self.assertNotEqual(AddNumbers().add_numbers(2), 11)
 
 if __name__ == '__main__':
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
